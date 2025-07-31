bfb2bd4925d2ec5175df6c4f52021630c140e707
anna <kokoan438@gmail.com>
2025-07-31 11:16:36 +0900

    add manage_orderfunc

--- ec_site/urls.py
+++ ec_site/urls.py
@@ -3,7 +3,7 @@ from . import views
 from ec_site import views
 urlpatterns = [
     path('product/list', views.listfunc, name='listfunc'),
-    path('product/<int:pk>/', views.detailfunc, name='detailfunc'),
+    path('product/<int:pk>', views.detailfunc, name='detailfunc'),
     path('product/cart/',views.view_cartfunc,name='view_cartfunc'),
     path('product/add/<int:pk>', views.add_cartfunc,name='add_cartfunc'),
     path('product/delete/<int:pk>', views.delete_cartfunc, name='delete_cartfunc'),
@@ -14,4 +14,6 @@ urlpatterns = [
     path('manage/create', views.manage_create, name='manage_create'),
     path('manage/edit/<int:pk>', views.manage_edit, name='manage_edit'),
     path('manage/delete/<int:pk>', views.manage_delete, name='manage_delete' ),
+    path('manage/orders', views.manage_order_list, name='manage_order_list'),
+    path('manage/orders/<int:pk>', views.manage_order_detail, name='manage_order_detail'),
 ]
