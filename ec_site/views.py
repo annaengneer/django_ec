96f012efa8a7621b89e4a240ca9958ea3edf25c7
anna <kokoan438@gmail.com>
2025-07-31 16:02:27 +0900

    fix  view.py ver7

--- ec_site/views.py
+++ ec_site/views.py
@@ -215,7 +215,7 @@ def upload_product(request):
 @basic_auth_required
 def manage_order_list(request):
     orders = Order.objects.all().order_by()
-    return render(request, 'order_list.html',{'orders':orders, 'items':items})
+    return render(request, 'order_list.html',{'orders':orders})
 
 def manage_order_detail(request, pk):
     order = get_object_or_404(Order, pk=pk)
